d0de9c82dc148d2e41c8823a8e2c8a43e62a4fa2
Wendley Santos <wendley.sdsilva@gmail.com>
2024-04-08 20:06:07 -0300

    pushing the project

---
+++ src/main/java/com/wendley/room/RoomSystemTestApplication.java
@@ -0,0 +1,13 @@
+package com.wendley.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSystemTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomSystemTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendley/room/RoomSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendley.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomSystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
