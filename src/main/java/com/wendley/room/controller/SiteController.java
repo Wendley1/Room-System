df0fd08c9e8a1b64e178053bbfe396d8d1b36cf7
Wendley Santos <wendley.sdsilva@gmail.com>
2024-04-08 20:51:36 -0300

    Setup data, repositories and controllers, created code generation logic

--- src/main/java/com/wendley/room/controller/SiteController.java
+++ src/main/java/com/wendley/room/controller/SiteController.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.wendley.room.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author Wendley S
  */
+@Controller
 public class SiteController {
-    
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 }
--- src/main/java/com/wendley/room/model/CodeGeneration.java
+++ src/main/java/com/wendley/room/model/CodeGeneration.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.wendley.room.model;
 
 /**
@@ -9,5 +5,20 @@ package com.wendley.room.model;
  * @author Wendley S
  */
 public class CodeGeneration {
+    private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static StringBuffer buffer;
+
+    public static String generateCodeRoomCode(Byte size) {
+        buffer = new StringBuffer(size);
+     
+        for(Byte i = 0; i < size; i++){
+            buffer.append(CHARS.charAt(getRandomNumber(0, 26)));
+        }
+
+        return buffer.toString();
+    }
     
+    public static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 }
