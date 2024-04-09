c70b1a559b2e164bade44fb39288d36ff900f10b
Wendley Santos <wendley.sdsilva@gmail.com>
2024-04-09 14:02:21 -0300

    Services

--- src/main/java/com/wendley/room/controller/UserRestController.java
+++ src/main/java/com/wendley/room/controller/UserRestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/user")
 public class UserRestController {
     
--- src/main/java/com/wendley/room/service/RoomService.java
+++ src/main/java/com/wendley/room/service/RoomService.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.wendley.room.service;
+
+import com.wendley.room.data.RoomEntity;
+import com.wendley.room.data.RoomRepository;
+import com.wendley.room.model.CodeGeneration;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Wendley S
  */
+@Service
 public class RoomService {
     
+    @Autowired
+    private RoomRepository repository;
+    
+    public RoomEntity createRoom(RoomEntity room) {
+        room.setId(null);
+        room.setCode(CodeGeneration.generateRoomCode((byte)6));
+        repository.save(room);
+        return room;
+    }
+
+    public RoomEntity findRoomById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public RoomEntity findRoomByCode(String code) {
+        return repository.findRoomByCode(code);
+    }
+
+    public List<RoomEntity> findAllRoons() {
+        return repository.findAll();
+    }
+    
+    public Integer getAmountOfUserInsideARoom(String roomCode){
+        return repository.amountOfUserInRoom(roomCode);
+    }
+
+    public Boolean joinRoom(Integer userId, Integer roomId){      
+        
+        if(userId == null || roomId == null)
+            throw new IllegalArgumentException("parameters cannot be null");
+        
+        return repository.insertUserInRoom(userId, roomId) == 0;
+    }
+    
+    public Boolean exitRoom(Integer userId, Integer roomId){      
+        
+        if(userId == null || roomId == null)
+            throw new IllegalArgumentException("parameters cannot be null");
+        
+        return repository.removeUserFromRoom(userId, roomId) == 1;
+    }
+    
+    public Boolean deleteRoom(Integer id) {
+        try {
+            repository.deleteById(findRoomById(id).getId());
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
 }
